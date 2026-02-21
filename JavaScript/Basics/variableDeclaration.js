b359185b37dca7b0c23fec0d9611da1cb032b1a4
GaganSingh2 <gks022004@gmail.com>
2026-02-21 17:05:58 +0530

    "Variable Declaration By Using var keyword"

---
+++ JavaScript/Basics/variableDeclaration.js
@@ -0,0 +1,21 @@
+//3 ways to Declare Variable---------
+//1) By Using var keyword
+var a //Declaration
+a = 10 //Initialization
+console.log(a);
+
+var b = 20 //Declaration + Initialization
+console.log(b);
+
+b = 30 //Re-Initialization
+console.log(b);
+
+var b //Re-Declaration
+
+var b = 40 //Re-Declaration + Re-Initialization
+console.log(b);
+
+
+
+
+
