ab015b37eedbc1c05869fda8c8a3c9b5412ec990
GaganSingh2 <gks022004@gmail.com>
2026-02-21 17:46:42 +0530

    "Script Scope"

---
+++ JavaScript/Basics/scopeTypes.js
@@ -0,0 +1,15 @@
+//Total 4 Scopes are there
+//1)Global Scope using var keyword
+console.log("Global Scope By Using var Keyword-----");
+var a = 10 //Global Scope
+console.log(a);
+
+
+//2)Script Scope By using let and const (declare at top of js)
+console.log("Script Scope By Using let and const keyword------");
+
+let b = 20 //Script Scope
+const c = 30 //Script Scope
+console.log(b);
+console.log(c);
+
