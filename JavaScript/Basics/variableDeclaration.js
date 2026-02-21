217f9378a9a2aee573142414e8622e321ca3f35c
GaganSingh2 <gks022004@gmail.com>
2026-02-21 17:21:46 +0530

    "Variable Declaration by Using let keyword"

--- JavaScript/Basics/variableDeclaration.js
+++ JavaScript/Basics/variableDeclaration.js
@@ -1,5 +1,7 @@
 //3 ways to Declare Variable---------
 //1) By Using var keyword
+console.log("By Using var keyword------");
+
 var a //Declaration
 a = 10 //Initialization
 console.log(a);
@@ -15,6 +17,21 @@ var b //Re-Declaration
 var b = 40 //Re-Declaration + Re-Initialization
 console.log(b);
 
+
+//2) By Using let keyword
+console.log("By Using let keyword-------");
+
+let x //Declaration
+x = 20 //Initialization
+console.log(x);
+
+x = 30 //Re-Initialization
+console.log(x);
+
+let y = 40 //Declaration + Initialization
+console.log(y);
+
+// let y //Re-Declaration, Re-Declaration + Initialization is not allowed
 
 
 
