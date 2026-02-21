83bfa5bae3f009c50daec7f855c229b1bdaae6e0
GaganSingh2 <gks022004@gmail.com>
2026-02-21 17:55:56 +0530

    "Function Scope"

--- JavaScript/Basics/scopeTypes.js
+++ JavaScript/Basics/scopeTypes.js
@@ -13,3 +13,18 @@ const c = 30 //Script Scope
 console.log(b);
 console.log(c);
 
+//3)Function Scope by Using var, let and const(declare inside the function)
+console.log("Function Scope By Using var,let and const keyword------");
+
+function test() {
+    var d = 40
+    let e = 50
+    const f = 60
+    console.log(d);
+    console.log(e);
+    console.log(f);
+}
+test()
+// console.log(d); //ReferenceError
+
+
