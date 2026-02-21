f86ee2c016276f1776c1ea15e71ea27263840f5f
GaganSingh2 <gks022004@gmail.com>
2026-02-21 18:25:02 +0530

    "imp points"

---
+++ JavaScript/Basics/AccessOfVariable.js
@@ -0,0 +1,16 @@
+a = 20 //By default become var type variable
+console.log(a); //20
+var a;
+
+// b = 40 //ReferenceError(not possible to access before initialization)
+// bcz If a variable is already initialized with some value, then only you can access it before the line in which it is declared.
+let b
+console.log(b); //undefined(default value for the var)
+
+b = 30
+console.log(b); //30
+
+
+
+
+
