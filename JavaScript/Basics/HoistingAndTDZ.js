7ef6e4500d2d1b739b718bfe04a4ce48b79fa39f
GaganSingh2 <gks022004@gmail.com>
2026-02-21 23:24:05 +0530

    "Hoisting and Temporal Dead Zone"

---
+++ JavaScript/Basics/HoistingAndTDZ.js
@@ -0,0 +1,10 @@
+//Hoisting-
+console.log(a); //Undefined (default value)
+var a = 29
+
+// console.log(b); //ReferenceError (currently it is in Temporal Dead Zone it can't access a before the initialization)
+// let b = 30
+
+
+// console.log(c); //ReferenceError (currently it is in Temporal Dead Zone it can't access a before the initialization)
+// const c = 30
