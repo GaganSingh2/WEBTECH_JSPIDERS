43d60aad68fa7fd99a45acdeac22350556344f3e
GaganSingh2 <gks022004@gmail.com>
2026-02-25 21:10:18 +0530

    "Data Types and Primitive Data Types"

---
+++ JavaScript/Basics/DataTypes.js
@@ -0,0 +1,25 @@
+//Primitive Data Type---------
+console.log("Primitive Data Types-----------");
+console.log("1) Number ");
+let a = 20
+console.log("Value of a: "+a+" and typeOf a: "+typeof a);
+
+console.log("2) Boolean ");
+let b = true
+console.log("Value of b: "+b+" and typeOf b: "+typeof b);
+
+console.log("3) String ");
+let c = 'Dishaa'
+console.log("Value of c: "+c+" and typeOf c: "+typeof c);
+
+console.log("4) Undefined ");
+let d
+console.log("Value of d: "+d+" and typeOf d: "+typeof d);
+
+console.log("5) BigInt ");
+let e = 20n //(suffix with n)
+console.log("Value of e: "+e+" and typeOf e: "+typeof e);
+
+console.log("6) Null ");
+let f = null
+console.log("Value of f: "+f+" and typeOf f: "+typeof f);
