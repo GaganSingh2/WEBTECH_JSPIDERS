384581df6c9437fb63bd73fd80fab67c482cf32d
GaganSingh2 <gks022004@gmail.com>
2026-02-26 21:38:19 +0530

    "Looping Statement and types"

---
+++ JavaScript/ConditionalAndControlStatement/LoopingStatement.js
@@ -0,0 +1,26 @@
+//Looping Statement------------
+//For Loop
+console.log("for loop----------");
+let v = 5
+for(let i=1; i<=10; i++){
+    console.log(v+" x "+i+" = "+v*i);
+}
+
+
+//while Loop
+console.log("while loop----------");
+v = 5
+let i = 1
+while(i<=10){
+    console.log(v+" x "+i+" = "+v*i);
+    i++
+}
+
+//do-while Loop
+console.log("do-while loop----------");
+v = 5
+i = 1
+do{
+    console.log(v+" x "+i+" = "+v*i);
+    i++
+}while(i<=10)
