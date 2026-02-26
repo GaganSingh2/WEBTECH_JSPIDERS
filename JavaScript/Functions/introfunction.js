7b841ab0210fa1621bc104c9f199a603866b891f
GaganSingh2 <gks022004@gmail.com>
2026-02-26 21:57:11 +0530

    "Intro of Function"

---
+++ JavaScript/Functions/introfunction.js
@@ -0,0 +1,16 @@
+function demo() {
+    console.log("Demo Function");
+}
+demo()
+
+
+hello()
+function hello() {
+    console.log("Hello Function");
+}
+
+// tmp() //if we call here then it will throw error bcz currently tmp store undefined and we try to call as function
+let tmp = function(){
+    console.log("Tmp Function");
+}
+tmp() //this one execute bcz currently tmp store Annonymous function 
