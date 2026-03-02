f81fa955795cb14b992b00559d90047ba03f186c
GaganSingh2 <gks022004@gmail.com>
2026-03-02 13:39:51 +0530

    "Arrow Function and its Characteristics"

--- JavaScript/Functions/ArrowFunction.js
+++ JavaScript/Functions/ArrowFunction.js
@@ -8,3 +8,17 @@ let demo = () => {
     console.log("Demo Hello");
 }
 demo() //here invoke using variableName()
+
+//Characteristics of Arrow Functions--------
+//1) If Arrow Function take One Argument then Parenthesis () is not required
+let hello = a => {
+    console.log("Value of A: "+a);
+}
+hello(2,5)
+
+
+//2) If Arrow Function take more than One Argument then Parenthesis () is required otherwise it will throw CTE
+let help = (a,b) => {
+    console.log("Sum of "+a+" and "+b+": "+(a+b));
+}
+help(5,5)
