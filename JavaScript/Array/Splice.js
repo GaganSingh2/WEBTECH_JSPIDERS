82067fc6b1d09b40d0fcbe79367ac061681f63c3
GaganSingh2 <gks022004@gmail.com>
2026-03-09 23:42:31 +0530

    "splice() in array"

---
+++ JavaScript/Array/Splice.js
@@ -0,0 +1,15 @@
+// splice(startIdx, deleteCount, addingElements)
+
+let arr = [10,20,30,40,50]
+
+// console.log(arr.splice(1));
+// console.log(arr.splice(1,2));
+// let res = arr.splice(1,2,100,200)
+// console.log(res);
+// console.log(arr);
+let res = arr.splice(6,0,100,200,300,400)
+console.log(res);
+console.log(arr);
+
+
+
