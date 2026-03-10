6893629484250a3747b9203f33d5a6b1f786bb0b
GaganSingh2 <gks022004@gmail.com>
2026-03-10 20:37:53 +0530

    "Reverse the String"

---
+++ JavaScript/Array/test.js
@@ -0,0 +1,16 @@
+// let arr = [110,150,200,450,750,900]
+// for(let i=0; i<arr.length; i++){
+//     arr[i] = arr[i]-(arr[i] * 10/100);
+// }
+
+// for(let ele of arr){
+//     console.log(ele);
+    
+// }
+
+
+
+// Q)Reverse the given string
+let str = "javascript"
+console.log(str.split("").reverse().join(""));
+// convert in array by using split(""), then reverse the array by using reverse() and then and convert in string using join("")
