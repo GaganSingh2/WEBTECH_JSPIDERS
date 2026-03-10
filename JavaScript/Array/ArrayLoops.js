9304c2600f6d130bdbbd5b260e045d7134c019d8
GaganSingh2 <gks022004@gmail.com>
2026-03-10 20:38:14 +0530

    "Array loops"

---
+++ JavaScript/Array/ArrayLoops.js
@@ -0,0 +1,15 @@
+let arr = [10,20,30,40,50]
+
+// forof loop to print the value
+console.log("Print the Value: ");
+for(let ele of arr){
+    console.log(ele);
+}
+
+
+// forin loop to print the indexes
+console.log("Print the index:");
+for(let idx in arr){
+    console.log(idx);
+    
+}
