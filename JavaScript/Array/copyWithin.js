e17b2fbd8cbf18af6ec0f85dfd7ae283807d955b
GaganSingh2 <gks022004@gmail.com>
2026-03-10 20:38:39 +0530

    "copyWithin"

---
+++ JavaScript/Array/copyWithin.js
@@ -0,0 +1,8 @@
+let arr = [10,20,30,40,50]
+
+// copyWithin(targetIdx, startIdx, endIdx)=> copy the elements from SI to EI and replace these elements from targetIdx
+// console.log(arr.copyWithin(1)); 
+
+// console.log(arr.copyWithin(1,3));
+
+console.log(arr.copyWithin(2,0,3));
