788ab123d33ba1fa8d40e5e27574d1d92cbc7f3e
GaganSingh2 <gks022004@gmail.com>
2026-03-11 23:16:35 +0530

    "Count the occurance of each character in give string"

--- JavaScript/Array/test.js
+++ JavaScript/Array/test.js
@@ -13,5 +13,15 @@
 
 // Q)Reverse the given string
 let str = "javascript"
-console.log(str.split("").reverse().join(""));
+// console.log(str.split("").reverse().join(""));
 // convert in array by using split(""), then reverse the array by using reverse() and then and convert in string using join("")
+
+// Q)FInd the occurance for each character
+let val = "we love javascript"
+
+console.log(
+    val.split("").reduce((acc,curr)=>{
+        acc[curr] = (acc[curr] || 0) +1
+        return acc
+    }, {})
+);
