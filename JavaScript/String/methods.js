23f0355f0d9688a67435e19f3cf16a324da49d37
GaganSingh2 <gks022004@gmail.com>
2026-03-13 14:09:13 +0530

    "toString() method"

--- JavaScript/String/methods.js
+++ JavaScript/String/methods.js
@@ -10,3 +10,13 @@ console.log("Before: "+str+" -> After: "+str.toLowerCase());
 console.log("toUpperCase() method------------------");
 let str1 = 'GagAn'
 console.log("Before: "+str1+" -> After: "+str1.toUpperCase());
+
+// 3)toString(): convert value to string--------------
+console.log("toString()----------------");
+let val = false
+console.log("Before using toString() typeOf of val: "+typeof val);
+let res = val.toString() //or let res = (234).toString()
+console.log("After using toString() typeOf of val: "+typeof res);
+
+
+
