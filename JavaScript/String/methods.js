25b24f8b57e3694a4f5aede9f0ce903c3b3bb7a3
GaganSingh2 <gks022004@gmail.com>
2026-03-13 15:44:16 +0530

    "trimStart(), trimEnd() and trim() method"

--- JavaScript/String/methods.js
+++ JavaScript/String/methods.js
@@ -17,6 +17,28 @@ let val = false
 console.log("Before using toString() typeOf of val: "+typeof val);
 let res = val.toString() //or let res = (234).toString()
 console.log("After using toString() typeOf of val: "+typeof res);
+// Note: we did not use toString() with null and undefined, it will cause TypeError
 
 
+// 4)trimStart(): remove spaces from start only------------
+console.log("trimStart()----------------");
+let btrim = ' hi'
+console.log("Before using trimStart() length of str: "+btrim.length);
+let atrim = btrim.trimStart()
+console.log("After using trimStart() length of str: "+atrim.length);
 
+
+// 5)trimStart(): remove spaces from end only------------
+console.log("trimEnd()----------------");
+btrim = 'hi '
+console.log("Before using trimEnd() length of str: "+btrim.length);
+atrim = btrim.trimEnd()
+console.log("After using trimEnd() length of str: "+atrim.length);
+
+
+// 6)trim(): remove spaces from start and end------------
+console.log("trim()----------------");
+btrim = ' hi '
+console.log("Before using trim() length of str: "+btrim.length);
+atrim = btrim.trim()
+console.log("After using trim() length of str: "+atrim.length);
