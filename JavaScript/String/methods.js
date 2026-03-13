70fb6adb1b89c502c2dde2f82cbb4488b5b7542a
GaganSingh2 <gks022004@gmail.com>
2026-03-13 14:02:35 +0530

    "toLowerCase() or toUpperCase() method"

---
+++ JavaScript/String/methods.js
@@ -0,0 +1,12 @@
+// Pre-Define Methods of String: all method return new String as result don't change in original string.
+
+//1) toLowerCase()-----------
+console.log("toLowerCase() method------------------");
+let str = 'GagAn'
+console.log("Before: "+str+" -> After: "+str.toLowerCase());
+
+
+//2) toUpperCase()-----------
+console.log("toUpperCase() method------------------");
+let str1 = 'GagAn'
+console.log("Before: "+str1+" -> After: "+str1.toUpperCase());
