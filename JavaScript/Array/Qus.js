429ef99e5a80b912211281a300f533e3f60412e0
GaganSingh2 <gks022004@gmail.com>
2026-03-15 00:53:37 +0530

    "Each value after 10% discount"

--- JavaScript/Array/Qus.js
+++ JavaScript/Array/Qus.js
@@ -1,1 +1,9 @@
-// Q1) 
+// Q1) For a given array with prices [120, 245, 300, 420, 150, 850] All items have an offer of 10% OFF on them. Change the array to store the final price after applying the offer.
+
+let prices = [120, 245, 300, 420, 150, 850]
+for(let idx in prices){
+    console.log("Before Discount: "+prices[idx]);
+    prices[idx] = prices [idx]- (prices[idx] * 10/100)
+     console.log("After 10% Discount: "+prices[idx]);
+}
+
