6e880cefd2161cfe84bd8212d32743420d57d4f3
GaganSingh2 <gks022004@gmail.com>
2026-03-15 00:45:04 +0530

    "for in() and for of() "

--- JavaScript/Array/ArrayLoops.js
+++ JavaScript/Array/ArrayLoops.js
@@ -13,3 +13,16 @@ let str = "Dishaaaa"
 for(let ch of str){
     console.log("Character: "+ch);
 }
+
+
+//2) for in()
+console.log("for in()----------");
+
+for(let idx in snacks){
+    console.log("Index: "+idx);
+}
+
+
+for(let idx in str){
+    console.log("Index: "+idx);
+}
