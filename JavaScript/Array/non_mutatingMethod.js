b2f64f4818d16bdd149b5d0583d192cfff2e4046
GaganSingh2 <gks022004@gmail.com>
2026-03-15 01:17:34 +0530

    "includes() and concat() method"

--- JavaScript/Array/non_mutatingMethod.js
+++ JavaScript/Array/non_mutatingMethod.js
@@ -21,4 +21,18 @@ console.log("lastIndexOf(2): "+arr.lastIndexOf(2));
 console.log("lastIndexOf(6): "+arr.lastIndexOf(6));
 
 
+// 4)includes(searchValue, startIndex): Checks if array contains a specified value or not
+console.log("includes(searchValue, startIndex)------------");
+console.log("includes(2): "+arr.includes(2));//check from 3 index
+console.log("includes(2,3): "+arr.includes(2,3));//check from 3 index
+console.log("includes(5): "+arr.includes(8));
 
+
+//5) concat(arr1,arr2,......,arr-n): add 2 or more array and return new array
+console.log("concat(arr1,arr2,......,arr-n)--------");
+let arr1 = [1,2]
+let arr2 = [3,4]
+console.log("Array1: "+arr1);
+console.log("Array2: "+arr2);
+let newArr = arr1.concat(arr2)
+console.log("New Array: "+newArr);
