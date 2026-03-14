a4c5f91265f6ba73d9e8bf7f92bca93110febf2f
GaganSingh2 <gks022004@gmail.com>
2026-03-14 16:02:01 +0530

    "Basic Operation"

--- JavaScript/Array/Operation.js
+++ JavaScript/Array/Operation.js
@@ -2,7 +2,7 @@
 let arr = new Array(5)
 
 console.log("Insertion in Array----------");
-arr[0] = 10
+// arr[0] = 10
 arr[1] = 'Gagan'
 arr[2] = 'Disha'
 arr[3] = 'Pawan'
@@ -12,7 +12,7 @@ console.log("Array after Insertion: "+arr);
 
 //2) Modification in Array
 console.log("Modification in Array----------");
-
+arr[0] = 10
 arr[3] = 40
 arr[4] = -10.9
 console.log("After Modification: "+arr);
@@ -21,6 +21,6 @@ console.log("After Modification: "+arr);
 //3)Deletion in Array
 console.log("Deletion in Array----------");
 
-delete arr[4]
+delete arr[1]
 delete arr[3]
 console.log("After Deletion: "+arr);
