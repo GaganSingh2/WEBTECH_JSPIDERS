c33c339895e341c2c1a194a62d0db01f5484f651
GaganSingh2 <gks022004@gmail.com>
2026-03-14 15:55:51 +0530

    "Basic Operation on Array"

---
+++ JavaScript/Array/Operation.js
@@ -0,0 +1,26 @@
+// 1) Insertion in Array---------------
+let arr = new Array(5)
+
+console.log("Insertion in Array----------");
+arr[0] = 10
+arr[1] = 'Gagan'
+arr[2] = 'Disha'
+arr[3] = 'Pawan'
+arr[4] = 10.9
+
+console.log("Array after Insertion: "+arr);
+
+//2) Modification in Array
+console.log("Modification in Array----------");
+
+arr[3] = 40
+arr[4] = -10.9
+console.log("After Modification: "+arr);
+
+
+//3)Deletion in Array
+console.log("Deletion in Array----------");
+
+delete arr[4]
+delete arr[3]
+console.log("After Deletion: "+arr);
