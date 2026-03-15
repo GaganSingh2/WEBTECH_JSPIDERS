05ab700ed8eccaf9b146a727c29dff4498d07684
GaganSingh2 <gks022004@gmail.com>
2026-03-16 01:30:43 +0530

    "separate all the string and number from given array"

--- JavaScript/Array/TestQus.js
+++ JavaScript/Array/TestQus.js
@@ -35,7 +35,7 @@ for(let left = 0; left<arr.length; left++){
 }
 console.log("After Moving 0;s: "+arr); //other appraoch is using filter() store the zero and non-zero in 2 diff array after that concat both array
 
-//Q3) Move all 0's to the first of array
+//Q4) Move all 0's to the first of array
 console.log("Q)Move all 0's to the first of array");
 arr = [0,2,0,4,0,1,0]
 console.log("Before Moving 0's: "+arr);
@@ -50,3 +50,36 @@ for(let right = arr.length-1; right>=0; right--){
     }
 }
 console.log("After Moving 0;s: "+arr);
+
+
+//Q5) Print the number and string separatoly from the given array
+console.log("Q) Print the number and string separatoly from the given array");
+
+arr = [10, "hello", 25, "Gagan", 30, "JS"];
+console.log("Before Separating: "+arr);
+
+let num = arr.filter((ele)=>{
+    return typeof ele === "number"
+})
+console.log("Number: "+num);
+
+let strr = arr.filter((ele)=>{
+    return typeof ele === "string"
+})
+console.log("String: "+strr);
+//Other approach=>
+console.log("Other approach---");
+
+let number = []
+let string = []
+for(let ele of arr){
+    if(typeof ele === 'number'){
+        number.push(ele)
+    }
+    else if(typeof ele === 'string'){
+        string.push(ele)
+    }
+}
+console.log("Number: "+number);
+console.log("String: "+string);
+
