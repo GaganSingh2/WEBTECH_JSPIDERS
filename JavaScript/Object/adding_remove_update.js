1b929062b0029198ed4dc5e65a360b4f40988934
GaganSingh2 <gks022004@gmail.com>
2026-03-17 16:14:58 +0530

    "Update the existing properties"

--- JavaScript/Object/adding_remove_update.js
+++ JavaScript/Object/adding_remove_update.js
@@ -1,11 +1,11 @@
-// Adding the properties-------------------
+//1) Adding the properties-------------------
 console.log("1)Adding the propertie---------------------");
 console.log("i)Using Dot Operator(.)-------");
 let user = {
     name: "Gagan"
 }
 console.log("Before Adding: ",user);
-user.skill = "JavaScript"
+user.skill = "JavaScript" //add new property
 console.log("After Adding: ",user);
 
 console.log("i)Using Bracket Notation([])-------");
@@ -13,5 +13,17 @@ let user1 = {
     name: "Gagan"
 }
 console.log("Before Adding: ",user1);
-user1["city"] = "Bangalore"
+user1["city"] = "Bangalore" //add new property
 console.log("After Adding: ",user1);
+
+
+//2) Updating the Properties------------------
+console.log("2)Updating the Propertie-------------");
+let user2 = {
+    name:"Gagan",
+    city:"Bangalore"
+}
+console.log("Before Updating: ",user2);
+user2.city = "Jaipur" //update the city
+console.log("After Updating: ",user2);
+
