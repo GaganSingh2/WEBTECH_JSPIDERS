d22d40deb444ccdc141bd6b4c2cfdac2d5470e81
GaganSingh2 <gks022004@gmail.com>
2026-03-17 16:11:33 +0530

    "Adding new Properties"

---
+++ JavaScript/Object/adding_remove_update.js
@@ -0,0 +1,17 @@
+// Adding the properties-------------------
+console.log("1)Adding the propertie---------------------");
+console.log("i)Using Dot Operator(.)-------");
+let user = {
+    name: "Gagan"
+}
+console.log("Before Adding: ",user);
+user.skill = "JavaScript"
+console.log("After Adding: ",user);
+
+console.log("i)Using Bracket Notation([])-------");
+let user1 = {
+    name: "Gagan"
+}
+console.log("Before Adding: ",user1);
+user1["city"] = "Bangalore"
+console.log("After Adding: ",user1);
