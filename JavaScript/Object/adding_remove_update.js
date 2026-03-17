d738e5486222db93b4b4f51fd6b2351c6e7b1fff
GaganSingh2 <gks022004@gmail.com>
2026-03-17 16:38:33 +0530

    "Deleting properties"

--- JavaScript/Object/adding_remove_update.js
+++ JavaScript/Object/adding_remove_update.js
@@ -25,5 +25,18 @@ let user2 = {
 }
 console.log("Before Updating: ",user2);
 user2.city = "Jaipur" //update the city
+// user2.age = 21 //if i try to update unavailable properties in the object then it is imlicitly add this property as a new Property
 console.log("After Updating: ",user2);
+
+
+//3)Deleting the Properties-------------
+console.log("3)Deleting the Properties--------------");
+let user3 = {
+    name:"Gagan",
+    city:"Bangalore"
+}
+console.log("Before Deleting: ",user3);
+delete user3.city
+// delete user3.age //nothing happened
+console.log("After Deleting: ",user3);
 
