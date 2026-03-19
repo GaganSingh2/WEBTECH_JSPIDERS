ad8becfe552e0bbd0a4b7ae9e2d257e69da593e0
GaganSingh2 <gks022004@gmail.com>
2026-03-19 14:46:23 +0530

    "Object.entries(), Object.assign() and Object.seal()"

--- JavaScript/Object/MethodOfObject.js
+++ JavaScript/Object/MethodOfObject.js
@@ -9,5 +9,61 @@ let user = {
     age: 22
 }
 let keys = Object.keys(user)
-console.log("Keys: "+keys);
+console.log("Keys: ",keys);
 
+
+// 2)Object.values(objName): used to get all the value in the array form
+console.log("2)Object.values(objName)-----------------");
+user = {
+    name: "Gagan",
+    city: "Bangalore",
+    age: 22,
+    skill: "Java"
+}
+let values = Object.values(user)
+console.log("Values: ",values);
+
+
+// 3)Object.entries(objName): used to get all the pairs[key,value] in the array form
+console.log("3)Object.entries(objName)-----------------");
+user = {
+    name: "Gagan",
+    city: "Bangalore",
+    age: 22,
+    skill: "Java"
+}
+let entries = Object.entries(user)
+console.log("Entries: ",entries);
+
+
+//4)Object.assign(targetObject,obj1, obj2, obj3......obj-n):copies properties from one or more source objects into a target object.
+console.log("Object.assign(targetObject,obj1, obj2, obj3......obj-n)----------------");
+let obj1 = {
+    name: "Gagan"
+}
+let obj2 = {
+    skill: "Java"
+}
+let obj3 = { 
+    city: "Bangalore"
+}
+Object.assign(obj3,obj1,obj2)//here, obj3 is targetObj means obj2 and obj1 store in obj3
+console.log("Properties: ",obj3);
+let targetObj = Object.assign({},obj1,obj2,obj3)//here, our tagetObj is new object({}) and remaining obj1,obj2 and obj3 are store in the new object and access through the objName
+console.log("Properties: ",targetObj);
+
+
+
+// 5)Object.seal(objName): prevent adding and deleting properties, but allows updating existing values.
+console.log("Object.seal(objName)--------------");
+let player = {
+    name: "Virat",
+    jersyNum: 18,
+    teamName: "RCB"
+}
+console.log("Before Updating: ",player);
+Object.seal(player)
+player.name = "Virat Kohli" //Updating allow
+player.age = 32 //adding new properties not allow
+delete player.name //deleting properties not allow
+console.log("After Updating: ",player);
