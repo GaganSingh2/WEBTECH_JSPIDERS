79e7103b1ca6bae551b22d31670b69e55fabbd8f
GaganSingh2 <gks022004@gmail.com>
2026-03-19 14:07:41 +0530

    "Object.keys()"

---
+++ JavaScript/Object/MethodOfObject.js
@@ -0,0 +1,13 @@
+// Method of Object----------------------------
+console.log("Method of Object------------------");
+
+// 1)Object.keys(objName): used to get all the keys in the array form
+console.log("1)Object.keys(objName)-----------------");
+let user = {
+    name: "Gagan",
+    city: "Bangalore",
+    age: 22
+}
+let keys = Object.keys(user)
+console.log("Keys: "+keys);
+
