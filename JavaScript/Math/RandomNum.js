9ed43dcf9921c67d1cb7af65579d1c361daae6d4
GaganSingh2 <gks022004@gmail.com>
2026-03-21 16:10:43 +0530

    "Generate Random numbers"

---
+++ JavaScript/Math/RandomNum.js
@@ -0,0 +1,26 @@
+// Q1)Generate the random number between 0 to 9
+console.log("Q1)Generate the random number between 0 to 9?");
+let num = Math.floor(Math.random() * 10)
+console.log("Number: "+num);
+let num2 = Math.floor(Math.random() * 10)
+console.log("Number: "+num2);
+let num3 = Math.floor(Math.random() * 10)
+console.log("Number: "+num3);
+let num4 = Math.floor(Math.random() * 10)
+console.log("Number: "+num4);
+let num5 = Math.floor(Math.random() * 10)
+console.log("Number: "+num5);
+
+
+// Q2)Generate the random number between 1 to 10
+console.log("Q2)Generate the random number between 1 to 10?");
+let n1 = Math.floor(Math.random() * 10)+1
+console.log("Number: "+n1);
+let n2 = Math.floor(Math.random() * 10)+1
+console.log("Number: "+n2);
+let n3 = Math.floor(Math.random() * 10)+1
+console.log("Number: "+n3);
+let n4 = Math.floor(Math.random() * 10)+1
+console.log("Number: "+n4);
+let n5 = Math.floor(Math.random() * 10)+1
+console.log("Number: "+n5);
