dd230b3c3fac96a48ed92b83905bc0c5a4ee36aa
GaganSingh2 <gks022004@gmail.com>
2026-03-21 14:43:47 +0530

    "Math.round()"

---
+++ JavaScript/Math/methodOfMath.js
@@ -0,0 +1,10 @@
+// Method of Math object---------------------
+console.log("Method of Math object---------------------------");
+
+// 1)Math.round(numericValue): it is used to round a number to the nearest integer. if value <0.5 then round down or if value >=0.5 then round up.
+console.log("1)Math.round(numericValue)-----------------");
+console.log("Math.round(4.6): "+Math.round(4.6)); //5
+console.log("Math.round(4.4): "+Math.round(4.4)); //4
+console.log("Math.round(4.5): "+Math.round(4.5)); //5
+
+
