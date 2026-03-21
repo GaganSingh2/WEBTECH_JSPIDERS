8ccef5dc115365add5327bc0860199da4de1a1db
GaganSingh2 <gks022004@gmail.com>
2026-03-21 15:02:36 +0530

    "Math.pow(),Math.floor(),Math.ceil()"

--- JavaScript/Math/methodOfMath.js
+++ JavaScript/Math/methodOfMath.js
@@ -6,5 +6,35 @@ console.log("1)Math.round(numericValue)-----------------");
 console.log("Math.round(4.6): "+Math.round(4.6)); //5
 console.log("Math.round(4.4): "+Math.round(4.4)); //4
 console.log("Math.round(4.5): "+Math.round(4.5)); //5
+console.log("Math.round(-4.4): "+Math.round(-4.4)); //-4
+console.log("Math.round(-4.5): "+Math.round(-4.5)); //-4
+console.log("Math.round(-4.6): "+Math.round(-4.6)); //-4
 
 
+// 2)Math.floor(number): round a number down to the nearest integer.
+console.log("Math.floor(number)------------------");
+console.log("math.floor(4.9): "+Math.floor(4.9));
+console.log("math.floor(-4.9): "+Math.floor(-4.9));
+console.log("math.floor(4.4): "+Math.floor(4.4));
+console.log("math.floor(-4.4): "+Math.floor(-4.4));
+
+
+// 3)Math.ceil(number): round a number up to the nearest integer.
+console.log("Math.ceil(number)------------------");
+console.log("math.ceil(4.9): "+Math.ceil(4.9));
+console.log("math.ceil(-4.9): "+Math.ceil(-4.9));
+console.log("math.ceil(4.4): "+Math.ceil(4.4));
+console.log("math.ceil(-4.4): "+Math.ceil(-4.4));
+
+
+// 4)Math.trunc(number): remove the decimal part of a number.
+console.log("Math.trunc(number)------------------");
+console.log("math.trunc(4.9): "+Math.trunc(4.9));
+console.log("math.trunc(-4.9): "+Math.trunc(-4.9));
+
+
+// 5)Math.pow(number,power): return the value of a number raised to a power.
+console.log("Math.pow(number,power)------------------");
+console.log("math.pow(2,3): "+Math.pow(2,3));
+console.log("math.pow(-4,2): "+Math.pow(-4,2));
+console.log("math.pow(-4,3): "+Math.pow(-4,3));
