9fb230498f82be1ffc2cedec856c6af5e35ae54d
GaganSingh2 <gks022004@gmail.com>
2026-03-21 14:27:07 +0530

    "Function Borrowing using call(),apply() and bind() to set this"

--- JavaScript/Object/functionBorrowing.js
+++ JavaScript/Object/functionBorrowing.js
@@ -16,3 +16,55 @@ let user2 = {
 user1.greet.call(user2) //Borrow greet function
 //user2 doesn’t have greet()
 // But it borrows it from user1
+
+
+// 2)Function Borrowing using apply() method
+console.log("Function Borrowing using apply() method---------------");
+let person1 = {
+    name: "Gagan",
+    greet: function(age,city){
+        console.log(`Hello ${this.name}, ${age}, ${city}`);
+    }
+}
+let person2 = {
+    name: "Amit"
+}
+person1.greet.apply(person2,[22,"Jaipur"])
+//person2 doesn’t have greet()
+// But it borrows it from person1
+
+
+// 3)Function Borrowing using bind() method
+console.log("Function Borrowing using bind() method---------------");
+let player1 = {
+    name:"KL Rahul",
+    greet: function(){
+        console.log(`Hello ${this.name} sir!`);
+    }
+}
+
+let player2 = {
+    name:"Virat Kohli"
+}
+// Create borrowed function
+let newFunc1 = player1.greet.bind(player2)
+newFunc1()
+//here, player2 does not have greet()
+//but it borrow it from the player1
+
+// other ex
+console.log("-------------------------------------");
+
+let player3 = {
+    name:"KL Rahul",
+    greet: function(teamName){
+        console.log(`Hello ${this.name} sir your teamName ${teamName}!`);
+    }
+}
+
+let player4 = {
+    name:"Virat Kohli"
+}
+let newFunc2 = player3.greet.bind(player4)
+newFunc2("RCB")
+newFunc2("India")
