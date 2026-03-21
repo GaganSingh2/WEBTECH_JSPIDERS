8dbaa777cccbd3766819f5016455abc5143529a0
GaganSingh2 <gks022004@gmail.com>
2026-03-21 14:26:29 +0530

    "Properties of Math Object"

---
+++ JavaScript/Math/propertiesOfMath.js
@@ -0,0 +1,21 @@
+// In-Built Properties in Math Object---------------
+console.log("In-Built Properties in Math Object-------------------");
+
+// 1)Math.PI: represent the ratio of the circumference of a circle to its diameter, approximately 3.14159
+console.log("1)Math.PI---------------");
+let pi = Math.PI
+console.log("Value of pi: "+pi);
+
+
+// 2)Math.SQRt1_2: reperesent the square root of 1/2, approximately 0.707
+console.log("2)Math.SQRT1_2---------------");
+let sqrt1_2 = Math.SQRT1_2
+console.log("Value of SQRT1_2: "+sqrt1_2);
+
+
+
+// 23)Math.SQRt2: reperesent the square root of 2, approximately 1.414
+console.log("3)Math.SQRT2---------------");
+let sqrt2 = Math.SQRT2
+console.log("Value of SQRT2: "+sqrt2);
+
