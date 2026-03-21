1523f8e35ece63ceda1790757e096c4dfb3428fd
GaganSingh2 <gks022004@gmail.com>
2026-03-21 14:27:54 +0530

    "Call() and bind() and apply()"

--- JavaScript/Object/call_bind_applyUsing_this.js
+++ JavaScript/Object/call_bind_applyUsing_this.js
@@ -18,7 +18,7 @@ let user = {
 }
 greet.call(user)
 
-//other waysss
+//other waysss:
 function mySelf(city,country){
      console.log(`My Name ${this.name} from ${city}, ${country}`); 
 }
@@ -28,11 +28,11 @@ let user2 = {
 console.log("Before using call() method:");
 mySelf(user2,"Bangalore","India")
 console.log("After using call() method:");
-mySelf.call(user2,"Bangalore","India")
+mySelf.call(user2,"Bangalore","India") //argument passed separatly to the function
 
 
-//1) Using apply(thisArg, [arg1, arg2,..........]):here argument passed in the form of array
-console.log("Using call(thisArg, [arg1, arg2,..........])---------------");
+//2) Using apply(thisArg, [arg1, arg2,..........]):here argument passed in the form of array
+console.log("Using apply(thisArg, [arg1, arg2,..........])---------------");
 function help(){
     console.log("Name: "+this.name);
 }
@@ -51,4 +51,34 @@ let self = {
 console.log("Before using apply(): ");
 selfIntro(self,"Bangalore","India")
 console.log("After using apply(): ");
-selfIntro.apply(self,["Bangalore","India"])
+selfIntro.apply(self,["Bangalore","India"])  //argument passed in the form of array to the function
+
+
+
+//3) Using bind(thisArg, arg1, arg2,..........):here argument passed separetly and return new Function so store in a variable and called later
+console.log("Using bind(thisArg, arg1, arg2,..........)---------------");
+function hello(){
+    console.log("Name: "+this.name);
+}
+let helloU = {
+    name: "Gagan"
+}
+let newFunction = hello.bind(helloU)
+newFunction()
+
+//other wayss
+function selfIntro1(city,country){
+    console.log(`my name is ${this.name} from ${city}, ${country}`);
+}
+let self1 = {
+    name:"Karan"
+}
+console.log("Before using bind(): ");
+selfIntro(self,"Bangalore","India")
+console.log("After using bind(): ");
+let newFunc = selfIntro.bind(self1,"Bangalore","India")  //argument passed separetly
+newFunc()
+
+let newFunc2 = selfIntro.bind(self1,"Bangalore")
+newFunc2()
+newFunc2("India")
