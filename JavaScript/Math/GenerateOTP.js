ed0922d8fba2e92d52a62b23b6c59432f537628e
GaganSingh2 <gks022004@gmail.com>
2026-03-21 16:11:02 +0530

    "Generate 4 Digit Otp"

---
+++ JavaScript/Math/GenerateOTP.js
@@ -0,0 +1,38 @@
+// Q1)Generate the 4 digit OTP
+console.log("Q1)Generate the 4 digit OTP?");
+let min = 1000
+let max = 9999
+
+
+// after this otp generate from 0 to 8999 instead of 1000 to 9999 it means some times it will generate otp like 98,43,532, but this otp's are break the qus bcz we want only 4-digit otp so for this reasone we add min at the end to ensure the generated otp only 4 digit
+// let otp = Math.floor(Math.random() * (max-min+1))
+// console.log("OTP: "+otp);
+
+
+
+let otp = Math.floor(Math.random() * (max-min+1)+min)
+console.log("OTP: "+otp);
+
+
+// How it works internally-----
+console.log("How it works Internally---------");
+
+// Step1: generate randome number
+console.log("Step1: generate randome number--------------");
+let otpD = Math.random()
+console.log("Step1 OTPD: "+otpD);
+
+// Step2: multiply the value by (max-min+1)
+console.log("Step2: multiply the value by (max-min+1)--------------");
+otpD = otpD * (max-min+1)
+console.log("Step2 OTPD: "+otpD);
+
+// Step3: add min at the end to ensure the 4 digit otp
+console.log("Step3: add min at the end to ensure the 4 digit otp--------------");
+otpD = otpD + min
+console.log("Step3 OTPD: "+otpD);
+
+// Step4: use Math.floor() to remove the decimal parts
+console.log("Step4: use Math.floor() to remove the decimal parts--------------");
+otpD = Math.floor(otpD)
+console.log("Step4 OTPD(final OTP): "+otpD);
