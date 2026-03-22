d3c31b0b8c6cd5c3eac772da8abaebb510f1ce4a
GaganSingh2 <gks022004@gmail.com>
2026-03-22 16:40:16 +0530

    "Creating Date and Time using parameters"

--- JavaScript/Date/CreatingDate_Time.js
+++ JavaScript/Date/CreatingDate_Time.js
@@ -12,3 +12,16 @@ console.log("Current Date & Time(LocalFormat): "+date.toLocaleString());
 console.log("Current Date(IST): "+date.toDateString());
 
 console.log("Current Time(IST): "+date.toTimeString());
+
+
+// Creating Date using parameters-----------
+console.log("Creating Date using parameters--------------------");
+let date1 = new Date(2026,1,22) //Month is 0-based(here 1 means February)
+console.log(date1);
+
+
+//Creating Date and Time using Parameter---------------
+//new Date(year,month,day,hours,minutes,seconds)
+console.log("Creating Date and time using parameters--------------------");
+let date2 = new Date(2026,1,22,2,13,56) //Month is 0-based(here 1 means February)
+console.log(date2);
