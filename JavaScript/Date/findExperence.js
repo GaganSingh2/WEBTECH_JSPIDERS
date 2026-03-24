b9195fdf037ca4b9d3352944196ecee6a79d3ed0
GaganSingh2 <gks022004@gmail.com>
2026-03-24 15:01:01 +0530

    "Calculate the experence"

--- JavaScript/Date/findExperence.js
+++ JavaScript/Date/findExperence.js
@@ -16,3 +16,26 @@ if(months<0){
 }
 
 console.log(`Total Experence: ${years} years and ${months} months and ${days} days`);
+
+
+// Q)Prabhat joined in a company on 12-april-2026 and left on 21-nov-2029. find the experience of prabhat in company.
+console.log("Q)Prabhat joined in a company on 12-april-2026 and left on 21-nov-2029. find the experience of prabhat in company.");
+
+
+let joinDate = new Date("2026-04-12")
+let leftDate = new Date("2029-11-21")
+
+let year = leftDate.getFullYear() - joinDate.getFullYear()
+let month = leftDate.getMonth() - joinDate.getMonth()
+let day = leftDate.getDate() - joinDate.getDate()
+
+if(day<0){
+    month--
+    day += 30
+}
+if(month<0){
+    year--
+    month += 12
+}
+
+console.log(`Total Experence: ${year} years and ${month} months and ${day} days`);
