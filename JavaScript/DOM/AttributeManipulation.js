f41b00e68ab5d04a697a033cb0ae9b2544c6df2c
GaganSingh2 <gks022004@gmail.com>
2026-06-08 15:21:16 +0530

    "Attribute Manipulation"

---
+++ JavaScript/DOM/AttributeManipulation.js
@@ -0,0 +1,6 @@
+let img = document.getElementById('myLaptop')
+console.log(img.getAttribute("src"));
+
+
+let img = document.getElementById('myLaptop')
+console.log(img.setAttribute("src", "acer.png"));
