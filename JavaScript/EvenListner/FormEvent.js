714ee4e5dd752da2c52479c59255e2e22b910b6f
GaganSingh2 <gks022004@gmail.com>
2026-06-27 11:51:58 +0530

    "Form Events"

--- JavaScript/EvenListner/FormEvent.js
+++ JavaScript/EvenListner/FormEvent.js
@@ -23,11 +23,38 @@ let form = document.querySelector("form")
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-    let user = document.querySelector("#user")
-    let pass = document.querySelector("#pass")
-    // console.dir(inp);
-    console.log("UserName: "+ user.value);
-    console.log("Password: "+pass.value);
+    //Creating Alert on the message-----------------
+    // let user = document.querySelector("#user")
+    // let pass = document.querySelector("#pass")
+    // // console.dir(inp);
+    // console.log("UserName: "+ user.value);
+    // console.log("Password: "+pass.value);
     
-    alert(`Hi ${user.value}, your password is set to ${pass.value}`)
+    // alert(`Hi ${user.value}, your password is set to ${pass.value}`)
+})
+
+let user = document.querySelector("#user")
+
+//Create function on evry submit or click on the submit button and screen button
+// user.addEventListener("change", function(){
+//     console.log("Change Events");
+//     console.log("Final Value: "+this.value);
+// })
+
+// //Create a function for each input type
+// user.addEventListener("input", function(){
+//     console.log("Input Event");
+//     console.log("Final Value: "+this.value);
+    
+    
+// })
+
+
+//Create a custome input on the screen
+let inp = document.querySelector("#text")
+let p = document.querySelector("p")
+
+inp.addEventListener("input", function(){
+    console.log("Final Value: "+inp.value);
+    p.innerText = inp.value //store in the input in the paragraph 
 })
