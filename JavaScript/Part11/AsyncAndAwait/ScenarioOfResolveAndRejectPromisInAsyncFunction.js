b5ca92c70305c250e6c1a0a54fdc9bcf6cb9d617
GaganSingh2 <gks022004@gmail.com>
2026-07-05 18:30:14 +0530

    "Scenario of Resolve and Reject Promise with Async Function"

---
+++ JavaScript/Part11/AsyncAndAwait/ScenarioOfResolveAndRejectPromisInAsyncFunction.js
@@ -0,0 +1,37 @@
+//All the Common scenarios, where i get Resolve and Reject Promise using Async Function?
+
+//Resolve Promise Scenarios-----------------
+//1) return inside async function -> Resolve Promise
+async function greet1() {
+    return "Resolved-1"
+}
+console.log(greet1());
+/**
+ * Output: Promise {<fulfilled>: 'Resolved'}
+ *         [PromiseState]: "fulfilled"
+ *         [PromiseResult]: "Resolved"
+ */
+
+//2)No return statement inside async function -> Resolve Promise with undefined
+async function greet2() {
+    return "Resolved-2"
+}
+console.log(greet2());
+/**
+ * Output: Promise {<fulfilled>: undefined}
+ *         [PromiseState]: "fulfilled"
+ *         [PromiseResult]: undefined
+ *  undefined bcz here didn't return statement inside the async function.
+ */
+
+//3)Return another Promise Explicitly that Resolve Promise
+async function greet3() {
+    return Promise.resolve("Resolved-3")
+}
+console.log(greet3());
+/**
+ * Output: Promise {<pending>: }//Pending bcz it is return Promise immediately not waiting for calling
+ *         [PromiseState]: "fulfilled"//Promise got resolve so after that it will return as fulfilled
+ *         [PromiseResult]: "Resolved-3"
+ *  
+ */
