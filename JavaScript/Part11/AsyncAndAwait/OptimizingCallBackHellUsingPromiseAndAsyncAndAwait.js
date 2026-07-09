a2f06722ca4a92407113732ac7c5501f0134a0d2
GaganSingh2 <gks022004@gmail.com>
2026-07-09 19:45:39 +0530

    "Optimize the CallBackHell Problem using Promise or Async and Await"

---
+++ JavaScript/Part11/AsyncAndAwait/OptimizingCallBackHellUsingPromiseAndAsyncAndAwait.js
@@ -0,0 +1,120 @@
+//Perfom Some Operation using CallBackHell-------------
+
+// function saveToDB(data, success, failure) {
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1 //1 to 10
+//     if (internetSpeed > 4) {
+//         // console.log("Your Data was Saved: "+data);
+//         success(data)
+//     } else {
+//         // console.log("Weak Connection! Data not saved!");
+//         failure()
+//     }
+
+// }
+
+
+// saveToDB("Apnaa College", 
+//     (data) => { //if this condition is true, then execute
+//         console.log("Success, Your Data was Saved: " + data);
+//         //then this inner function will execute
+//         saveToDB("Gagan", 
+//             //if this condition is true, then execute
+//             (data) => {
+//                 console.log("Success, Your Data was Saved: " + data);
+                
+//                 saveToDB("Dishaa", 
+//                     //if this condition is true, then execute
+//                     (data)=>{
+//                         console.log("Success, Your Data was Saved: " + data);
+//                     },
+//                     //otherwise this function will execute
+//                     ()=>{
+//                         console.log("Failure, Weak Connection! Data not saved!");
+//                     }
+//                 )
+//             }, 
+//             //otherwise this function will execute
+//             () => {
+//                 console.log("Failure, Weak Connection! Data not saved!");
+//             }
+//         )
+//     }, 
+//     //Otherwise this function will execute
+//     () => {
+//         console.log("Failure, Weak Connection! Data not saved!");
+//     }
+// )
+
+//this situation is called as CallBack Hell.
+//to overcome this problem we used Promises Chaining.
+
+
+//Overcome CallBack Hell using Promise Chaining--------------
+// function saveToDB(data){
+//     return new Promise((resolve, reject)=>{
+//         let internetSpeed = Math.floor(Math.random()*10)+1
+//         if(internetSpeed > 4){
+//             resolve("Success: Data "+data+" Was Saved!")
+//         }
+//         else{
+//             reject("Failed: Weak Connection!")
+//         }
+//     })
+// }
+
+
+// let req = saveToDB("Apna College")
+// req.then((result)=>{
+//     console.log(result);
+//     return saveToDB("Gagan")
+// })
+// .then((result)=>{
+//     console.log(result);
+//     return saveToDB("Dishaaaa")
+// })
+// .then((result)=>{
+//     console.log(result+" In Heart");
+// })
+// // .then(()=>{ //we can directly throw our own error/exception
+// //     throw "Database Error!"
+// // })
+// .catch((error)=>{
+//     console.log(error);
+    
+// })
+
+//To Optimize the Promise Chaining using Async And Await--------------
+function saveToDB(data){
+    return new Promise((resolve, reject)=>{
+        let internetSpeed = Math.floor(Math.random() * 10) + 1
+        if(internetSpeed > 4){
+            resolve("Success: Data "+data+" Was Saved!")
+        }
+        else{
+            reject("Failed: Weak Connection!")
+        }
+    })
+}
+
+async function user() {
+    try{
+        // Multiple await calls (replacement for Promise Chaining)
+        let req1 = await saveToDB("Dishuuu")
+        console.log(req1);
+
+        let req2 = await saveToDB("Gagan")
+        console.log(req2);
+
+        let req3 = await saveToDB("Karan")
+        console.log(req3);
+
+        let req4 = await saveToDB("Vishal")
+        console.log(req4);
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+
+}
+user()
